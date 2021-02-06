85ccebb49597644d7aa8ece7c061d66e5a95313f
Vai <vaishali2016@outlook.com>
2021-02-06 13:14:03 +0000

    [Server] formatted itemodel file

--- Models/Item.js
+++ Models/Item.js
@@ -1,26 +1,24 @@
+const mongoose = require("mongoose");
 
-const mongoose = require('mongoose')
+mongoose
+  .connect("mongodb://localhost:27017/ToDoList")
+  .then(console.log("connected"));
 
-mongoose.connect('mongodb://localhost:27017/ToDoList').then(console.log("connected"))
+const Schema = mongoose.Schema;
 
-const Schema=mongoose.Schema
+const ItemSchema = new Schema({
+  Title: {
+    type: String,
+    required: true,
+  },
 
-const ItemSchema=new Schema({
-	Title: {
-		type: String,
-		required: true
-	}, 
+  Description: {
+    type: String,
+  },
 
-	Description: {
-		type: String
-	},
+  Due: {
+    type: Date,
+  },
+});
 
-	Due: {
-		type: Date
-	}
-	
-	
-
-})
-
-module.exports.ItemModel = mongoose.model('Item', ItemSchema)
+module.exports.ItemModel = mongoose.model("Item", ItemSchema);
