15d9ede247afa51b6a1a90dc25f9c0a954bf9b0b
Vai <vaishali2016@outlook.com>
2021-02-06 14:44:56 +0000

    [Server] restful services done for crud operations

--- app.js
+++ app.js
@@ -14,3 +14,37 @@ app.use(
     extended: true,
   })
 );
+
+const {
+  saveItem,
+  findItems,
+  updateItem,
+  deleteItem,
+} = require("./DAO/ItemDAO");
+
+app.get("/items/view", async function (req, res) {
+  const items = await findItems();
+  res.json(items);
+});
+
+app.post("/items/create", async function (req, res) {
+  const title = req.body.title;
+  const description = req.body.description;
+  const due = req.body.due;
+
+  await saveItem(title, description, due);
+});
+
+app.put("/items/update/:id", async function (req, res) {
+  const id = req.params.id;
+  const title = req.body.title;
+  const description = req.body.description;
+  const due = req.body.due;
+
+  await updateItem(id, title, description, due);
+});
+
+app.delete("/items/delete/:id", async function (req, res) {
+  const id = req.params.id;
+  await deleteItem(id);
+});
